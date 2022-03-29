4edd7989eb62a7f23139c533b0e43f172c74409e
Kyle Corlett <corlettkyle5@hotmail.com>
2022-03-30 09:56:46 +1000

    Added structures for the breakdown of ingredients and directions

--- Cookcademy/Recipe.swift
+++ Cookcademy/Recipe.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+struct Recipe {
+    var mainInformation: MainInformation
+    var ingredients: [String]
+    var directions: [String]
+}
+
+struct MainInformation{
+    var name: String
+    var description: String
+    var author: String
+    var category: String //Breakfast, Lunch, Dinner, Dessert
+    
+    enum Category: String, CaseIterable {
+        case breakfast = "Breakfast"
+        case lunch = "Lunch"
+        case dinner = "Dinner"
+        case dessert = "Dessert"
+    }
+}
+
+struct Ingredient {
+    var name: String
+    var quantity: Double
+    var unit: String //Ounces, Grams, Cups, Tablespoons, Teaspoons, None
+}
+
+struct Direction{
+    var description: String
+    var isOptional: Bool
+}
