574a749e6498bac62e75d5719531b9ca03005363
Kyle Corlett <corlettkyle5@hotmail.com>
2022-03-30 09:42:15 +1000

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Kyle-Joshua Corlett on 30/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
