09fa9440a6bb5fc4e08fac3ace90b46501609aec
Kyle Corlett <corlettkyle5@hotmail.com>
2022-03-30 10:15:48 +1000

    Added way for singular amounts to have the plural 'S' removed off the name:String

--- Cookcademy/Recipe.swift
+++ Cookcademy/Recipe.swift
@@ -30,15 +30,33 @@ struct MainInformation{
 struct Ingredient {
     var name: String
     var quantity: Double
-    var unit: String //Ounces, Grams, Cups, Tablespoons, Teaspoons, None
-    
-    enum Unit: String, CaseIterable{
+    var unit: Unit
+   
+    var description: String {
+      let formattedQuantity = String(format: "%g", quantity)
+      switch unit {
+      case .none:
+        let formattedName = quantity == 1 ? name : "\(name)s"
+        return "\(formattedQuantity) \(formattedName)"
+      default:
+        if quantity == 1 {
+          return "1 \(unit.singularName) \(name)"
+          else {
+            return "\(formattedQuantity) \(unit.rawValue) \(name)"
+          }
+        }
+      }
+   
+      enum Unit: String, CaseIterable {
         case oz = "Ounces"
         case g = "Grams"
         case cups = "Cups"
         case tbs = "Tablespoons"
-        case tps = "Teaspoons"
-        case none = "No units"
+        case tsp = "Teaspoons"
+        case none = "No Units"
+   
+        var singularName: String { String(rawValue.dropLast()) }
+      }
     }
 }
 
@@ -46,3 +64,4 @@ struct Direction{
     var description: String
     var isOptional: Bool
 }
+
