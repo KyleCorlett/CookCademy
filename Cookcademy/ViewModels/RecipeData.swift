c820a895ef88cb19723046543ce9e2bae7f6c8a2
Kyle Corlett <corlettkyle5@hotmail.com>
2022-04-03 11:02:55 +1000

    Cleaned up is valid statement to flow more with MVVM

--- Cookcademy/ViewModels/RecipeData.swift
+++ Cookcademy/ViewModels/RecipeData.swift
@@ -19,8 +19,11 @@ class RecipeData: ObservableObject {
         }
         return filteredRecipes
     }
+    func add(recipe: Recipe){
+        if recipe.isValid {
+            recipes.append(recipe)
+        }
+    }
 }
 
-class RecipesData: ObservableObject {
-    @Published var recipes = Recipe.testRecipes
-}
+
